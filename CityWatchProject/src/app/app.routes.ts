dea8d26b38dde5c6c955a0cccb161a96a0d6dc94
mik3ymikes <113144829+mik3ymikes@users.noreply.github.com>
2024-02-16 17:14:35 -0600

    working on routes

--- CityWatchProject/src/app/app.routes.ts
+++ CityWatchProject/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { AlertsComponent } from './alerts/alerts.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  // {path:'', component:}
+  {path:'alerts', component:AlertsComponent}
+];
