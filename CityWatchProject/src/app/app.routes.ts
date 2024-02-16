9f1bc23f9fc269296c0f242ff2bf3d3e1b1fe4e7
mik3ymikes <113144829+mik3ymikes@users.noreply.github.com>
2024-02-16 17:24:54 -0600

    still trying to figure out routes

--- CityWatchProject/src/app/app.routes.ts
+++ CityWatchProject/src/app/app.routes.ts
@@ -2,6 +2,8 @@ import { Routes } from '@angular/router';
 import { AlertsComponent } from './alerts/alerts.component';
 
 export const routes: Routes = [
+  // { path: '', redirectTo: 'home', pathMatch: 'full'},
+  // { path: 'home', component: HomeComponent },
   // {path:'', component:}
   {path:'alerts', component:AlertsComponent}
 ];
