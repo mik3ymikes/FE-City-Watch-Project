e8c8f1321e5e87ae57c5ba9f0d885dafac18c873
mik3ymikes <113144829+mik3ymikes@users.noreply.github.com>
2024-02-18 22:32:33 -0600

    signu up and login set up

--- CityWatchProject/src/app/app.routes.ts
+++ CityWatchProject/src/app/app.routes.ts
@@ -4,12 +4,17 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
 import { HomeComponent } from './home/home.component';
 import { StatisticsComponent } from './statistics/statistics.component';
 import { EventzComponent } from './eventz/eventz.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+
 
 export const routes: Routes = [
   { path: 'home', component: HomeComponent },
   {path:'events', component: EventzComponent},
   {path:'alerts', component:AlertsComponent},
   {path: 'statistics', component:StatisticsComponent},
+  {path: 'login', component:LoginComponent},
+  {path: 'signup', component:SignupComponent},
   { path: '', redirectTo: '/home', pathMatch: 'full'},
   { path: '**', component: PageNotFoundComponent}
 ];
