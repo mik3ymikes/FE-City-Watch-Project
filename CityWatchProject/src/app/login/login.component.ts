1af07b41a98439e6b43f3f76204a6474c17c8936
mik3ymikes <113144829+mik3ymikes@users.noreply.github.com>
2024-02-19 22:54:28 -0600

    added login functoin

--- CityWatchProject/src/app/login/login.component.ts
+++ CityWatchProject/src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -8,5 +10,46 @@ import { Component } from '@angular/core';
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  isError:boolean=false
+  constructor(private router:Router){}
+
+
+
+  loginForm: FormGroup=new FormGroup({
+    username:new FormControl('', Validators.required),
+    password:new FormControl('', Validators.required),
+
+
+
+
+
+  })
+
+close(){
+  this.router.navigate(['home']);
+}
+
 
 }
+//   login(){
+//     if(this.loginForm.valid){
+//       const username=this.loginForm.value.username;
+//       const password=this.loginForm.value.password;
+
+//       this.authService.login(username, password).subscribe({
+//         next: (res:any)=>{
+//               console.log(res);
+//               this.authService.setToken(res.token)
+//               this.router.navigate(['/'])
+//         },
+//         error: (error:any) =>{
+//           console.log("Error when logging in", error )
+//           this.isError=true
+//         }
+//       })
+//     }
+//   }
+// }
+
+
+
