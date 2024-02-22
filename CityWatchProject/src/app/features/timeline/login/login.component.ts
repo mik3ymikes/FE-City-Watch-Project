ac8563815efe046bb80d421cd6e8b8f784e35b59
mik3ymikes <113144829+mik3ymikes@users.noreply.github.com>
2024-02-22 16:42:09 -0600

    added a login

--- CityWatchProject/src/app/features/timeline/login/login.component.ts
+++ CityWatchProject/src/app/features/timeline/login/login.component.ts
@@ -1,29 +1,49 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { AuthenticationService } from '../../../core/services/authentication.service';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
+
+
 export class LoginComponent {
-  isError:boolean=false
-  constructor(private router:Router){}
-
-
-
   loginForm: FormGroup=new FormGroup({
     username:new FormControl('', Validators.required),
     password:new FormControl('', Validators.required),
 
+      })
 
 
+  isError:boolean=false
 
 
-  })
+ constructor(private authService:AuthenticationService, private router:Router){}
+
+ login(){
+   if(this.loginForm.valid){
+     const username=this.loginForm.value.username;
+     const password=this.loginForm.value.password;
+
+     this.authService.login(username, password).subscribe({
+       next: (res:any)=>{
+             console.log(res);
+             this.authService.setToken(res.token)
+             this.router.navigate(['/'])
+       },
+       error: (error:any) =>{
+         console.log("Error when logging in", error )
+         this.isError=true
+       }
+     })
+   }
+ }
+
 
 close(){
   this.router.navigate(['home']);
@@ -31,25 +51,7 @@ close(){
 
 
 }
-//   login(){
-//     if(this.loginForm.valid){
-//       const username=this.loginForm.value.username;
-//       const password=this.loginForm.value.password;
 
-//       this.authService.login(username, password).subscribe({
-//         next: (res:any)=>{
-//               console.log(res);
-//               this.authService.setToken(res.token)
-//               this.router.navigate(['/'])
-//         },
-//         error: (error:any) =>{
-//           console.log("Error when logging in", error )
-//           this.isError=true
-//         }
-//       })
-//     }
-//   }
-// }
 
 
 
