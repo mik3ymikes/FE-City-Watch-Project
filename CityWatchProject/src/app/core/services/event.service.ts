4bd2ded344076135863957cd49df8adde8f29a0e
mik3ymikes <113144829+mik3ymikes@users.noreply.github.com>
2024-02-22 20:07:38 -0600

    set up service

--- CityWatchProject/src/app/core/services/event.service.ts
+++ CityWatchProject/src/app/core/services/event.service.ts
@@ -1,9 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EventService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+
+  getEvents():Observable <Event[]>{
+    return this.http.get<Event[]>(`${environment.apiUrl}/events`)
+  }
 }
+
