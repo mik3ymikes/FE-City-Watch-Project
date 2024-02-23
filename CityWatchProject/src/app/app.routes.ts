64c7735b236dd5969d9979e0d22152f0b4b1905a
mik3ymikes <113144829+mik3ymikes@users.noreply.github.com>
2024-02-22 20:16:31 -0600

    changed route

--- CityWatchProject/src/app/app.routes.ts
+++ CityWatchProject/src/app/app.routes.ts
@@ -1,9 +1,9 @@
 import { Routes } from '@angular/router';
-import { AlertComponent } from './shared/components/alert/alert.component';
+
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { HomeComponent } from './home/home.component';
 import { StatisticsComponent } from './statistics/statistics.component';
-import { EventzComponent } from './shared/components/eventz/eventz.component';
+
 import { LoginComponent } from './features/timeline/login/login.component';
 import { SignupComponent } from './signup/signup.component';
 import { authGuard } from './core/guards/auth.guard';
@@ -13,8 +13,8 @@ import { noAuthGuard } from './core/guards/no-auth.guard';
 export const routes: Routes = [
   { path: 'home', component: HomeComponent},
   {path:'events', loadComponent: ()=>
-  import("./shared/components/eventz/eventz.component").then((c) =>
-   c.EventzComponent), canActivate: [authGuard] },
+  import("./features/eventz-display/eventz-display.component").then((c) =>
+   c.EventzDisplayComponent), canActivate: [authGuard] },
   {path:'alerts', loadComponent: ()=>
   import("./features/timeline/timeline.component").then((c) =>
    c.TimelineComponent), canActivate: [authGuard] },
