3b76bebcb10802f458550750e60586d3ed16c899
mik3ymikes <113144829+mik3ymikes@users.noreply.github.com>
2024-02-23 16:13:45 -0600

    commented pagination out will need to check bakc if try to fix

--- CityWatchProject/src/app/pagination/pagination.component.ts
+++ CityWatchProject/src/app/pagination/pagination.component.ts
@@ -15,46 +15,50 @@ import { Event } from '../shared/models/event';
 
 
 export class PaginationComponent implements OnInit {
-  items: Event[] = [];
-  currentPage: number = 1;
-  itemsPerPage: number = 10;
-
-  constructor(private eventService:EventService){}
 
   ngOnInit(): void {
-    this.eventService.getEvents().subscribe({
-      next:(items:Event[])=>{
-        this.items=items
-      },
-      error: (error:any) =>{
-        console.error("Error fetching timeline items", error)
-      }
-    })
-
-
+      
   }
+  // items: Event[] = [];
+  // currentPage: number = 1;
+  // itemsPerPage: number = 10;
+
+  // constructor(private eventService:EventService){}
+
+  // ngOnInit(): void {
+  //   this.eventService.getEvents().subscribe({
+  //     next:(items:Event[])=>{
+  //       this.items=items
+  //     },
+  //     error: (error:any) =>{
+  //       console.error("Error fetching timeline items", error)
+  //     }
+  //   })
+
+
+  // }
 
 
 
-  get paginatedItems() {
-    const startIndex = (this.currentPage - 1) * this.itemsPerPage;
-    const endIndex = startIndex + this.itemsPerPage;
-    return this.items.slice(startIndex, endIndex);
-  }
+  // get paginatedItems() {
+  //   const startIndex = (this.currentPage - 1) * this.itemsPerPage;
+  //   const endIndex = startIndex + this.itemsPerPage;
+  //   return this.items.slice(startIndex, endIndex);
+  // }
 
-  onPageChange(pageNumber: number) {
-    this.currentPage = pageNumber;
-  }
+  // onPageChange(pageNumber: number) {
+  //   this.currentPage = pageNumber;
+  // }
 
-  pages(): number[] {
-    const totalItems = this.items.length;
-  const totalPages = Math.ceil(totalItems / this.itemsPerPage);
-    const pagesArray = Array.from({ length: totalPages }, (_, i) => i + 1);
-    return pagesArray;
-  }
+  // pages(): number[] {
+  //   const totalItems = this.items.length;
+  // const totalPages = Math.ceil(totalItems / this.itemsPerPage);
+  //   const pagesArray = Array.from({ length: totalPages }, (_, i) => i + 1);
+  //   return pagesArray;
+  // }
 
-  totalPages(): number {
-    return this.pages().length;
-  }
+  // totalPages(): number {
+  //   return this.pages().length;
+  // }
 
 }
