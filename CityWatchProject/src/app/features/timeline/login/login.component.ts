e39fcfbf475e67fd74df610aa58b9f95ae448dfb
mik3ymikes <113144829+mik3ymikes@users.noreply.github.com>
2024-02-26 15:06:00 -0600

    set up login spinner component

--- CityWatchProject/src/app/features/timeline/login/login.component.ts
+++ CityWatchProject/src/app/features/timeline/login/login.component.ts
@@ -2,17 +2,21 @@ import { Component } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthenticationService } from '../../../core/services/authentication.service';
+import { LoadingSpinnerComponent } from '../../../shared/loading-spinner/loading-spinner.component';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, LoadingSpinnerComponent],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 
 
 export class LoginComponent {
+
+  isLoading=false
+
   loginForm: FormGroup=new FormGroup({
     username:new FormControl('', Validators.required),
     password:new FormControl('', Validators.required),
@@ -29,6 +33,7 @@ export class LoginComponent {
    if(this.loginForm.valid){
      const username=this.loginForm.value.username;
      const password=this.loginForm.value.password;
+     this.isLoading=true
 
      this.authService.login(username, password).subscribe({
        next: (res:any)=>{
@@ -39,6 +44,7 @@ export class LoginComponent {
        error: (error:any) =>{
          console.log("Error when logging in", error )
          this.isError=true
+         this.isLoading=false
        }
      })
    }
