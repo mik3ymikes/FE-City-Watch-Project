085cc7cdbe064c2b28f6e5c7c99874aaa5ff5c58
mik3ymikes <113144829+mik3ymikes@users.noreply.github.com>
2024-02-26 13:57:16 -0600

    set up pagination for others if able

--- CityWatchProject/src/app/pagination/pagination.component.ts
+++ CityWatchProject/src/app/pagination/pagination.component.ts
@@ -1,12 +1,13 @@
-// import { NgFor } from '@angular/common';
+
 // import { Component, Input, OnInit} from '@angular/core';
 // import { EventService } from '../core/services/event.service';
 // import { Event } from '../shared/models/event';
+// import { ActivatedRoute, Router } from '@angular/router';
 
 // @Component({
 //   selector: 'app-pagination',
 //   standalone: true,
-//   imports: [NgFor],
+//   imports: [],
 //   templateUrl: './pagination.component.html',
 //   styleUrl: './pagination.component.css'
 // })
@@ -16,47 +17,62 @@
 
 // export class PaginationComponent implements OnInit {
 
+// events: Event[]=[]
+// currentPage: number = 1;
+// itemsPerPage: number = 18;
 
-//   items: Event[] = [];
-//   currentPage: number = 1;
-//   itemsPerPage: number = 10;
 
-//   constructor(private eventService:EventService){}
 
-//   ngOnInit(): void {
-//     this.eventService.getEvents().subscribe({
-//       next:(items:Event[])=>{
-//         this.items=items
-//       },
-//       error: (error:any) =>{
-//         console.error("Error fetching timeline items", error)
-//       }
+
+// constructor(private eventService:EventService,
+//   private router:Router, private route:ActivatedRoute){}
+
+// ngOnInit(): void {
+
+
+//   this.route.queryParams.subscribe(params=>{
+//       this.currentPage=params['page'] ? Number(params['page']) :1
+
+
 //     })
 
+//   this.eventService.getEvents().subscribe({
+//     next: (events:Event[])=>{
+//       this.events=events
+//     },
+//     error: (error:any) =>{
+//       console.error("Error fetching timeline events", error)
+//     }
+//   })
 
-//   }
+
+// }
 
 
 
-//   get paginatedItems() {
-//     const startIndex = (this.currentPage - 1) * this.itemsPerPage;
-//     const endIndex = startIndex + this.itemsPerPage;
-//     return this.items.slice(startIndex, endIndex);
-//   }
 
-//   onPageChange(pageNumber: number) {
-//     this.currentPage = pageNumber;
-//   }
+// get paginatedItems() {
+//   const startIndex = (this.currentPage - 1) * this.itemsPerPage;
+//   const endIndex = startIndex + this.itemsPerPage;
+//   return this.events.slice(startIndex, endIndex);
+// }
 
-//   pages(): number[] {
-//     const totalItems = this.items.length;
-//   const totalPages = Math.ceil(totalItems / this.itemsPerPage);
-//     const pagesArray = Array.from({ length: totalPages }, (_, i) => i + 1);
-//     return pagesArray;
-//   }
+// onPageChange(pageNumber: number) {
+//   this.currentPage = pageNumber;
+//   this.router.navigate([], {
+//     relativeTo:this.route,
+//     queryParams: {page:this.currentPage},
+//     queryParamsHandling: 'merge'
+// }) }
 
-//   totalPages(): number {
-//     return this.pages().length;
-//   }
+// pages(): number[] {
+//   const totalItems = this.events.length;
+// const totalPages = Math.ceil(totalItems / this.itemsPerPage);
+//   const pagesArray = Array.from({ length: totalPages }, (_, i) => i + 1);
+//   return pagesArray;
+// }
 
+// totalPages(): number {
+//   return this.pages().length;
+// }
 // }
