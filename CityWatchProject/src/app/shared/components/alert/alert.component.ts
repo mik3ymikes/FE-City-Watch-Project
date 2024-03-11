adf8c272200ab23f6c4a25db56ad62905fa3fee0
mik3ymikes <113144829+mik3ymikes@users.noreply.github.com>
2024-03-11 15:03:36 -0500

    styling

--- CityWatchProject/src/app/shared/components/alert/alert.component.ts
+++ CityWatchProject/src/app/shared/components/alert/alert.component.ts
@@ -1,14 +1,23 @@
 import { Component, Input } from '@angular/core';
 import { Alert } from '../../models/alert';
-import { DatePipe } from '@angular/common';
+import { DatePipe, NgFor, NgStyle } from '@angular/common';
 
 @Component({
   selector: 'app-alert',
   standalone: true,
-  imports: [DatePipe],
+  imports: [DatePipe, NgStyle, NgFor],
   templateUrl: './alert.component.html',
   styleUrl: './alert.component.css'
 })
 export class AlertComponent {
 @Input({required:true}) alert:Alert=new Alert({})
+
+
+
+// getColor(index:number): string {
+//   // Use the modulus operator to determine the color based on the index
+//   const colors = ['bg-dark', 'bg-primary', 'bg-success', 'bg-info', 'bg-warning', 'bg-danger'];
+//   return colors[index % colors.length];
+
+// }
 }
