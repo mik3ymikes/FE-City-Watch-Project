817e07f459560eca6868a3c9a3ffa603b879675a
mik3ymikes <113144829+mik3ymikes@users.noreply.github.com>
2024-03-18 14:50:24 -0500

    setting up viewing participants

--- CityWatchProject/src/app/view-attendees/view-attendees.component.ts
+++ CityWatchProject/src/app/view-attendees/view-attendees.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Event } from '../shared/models/event';
+import { EventService } from '../core/services/event.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-view-attendees',
@@ -7,6 +10,29 @@ import { Component } from '@angular/core';
   templateUrl: './view-attendees.component.html',
   styleUrl: './view-attendees.component.css'
 })
-export class ViewAttendeesComponent {
+export class ViewAttendeesComponent implements OnInit {
+event:Event = new Event ()
+
+
+constructor(private route:ActivatedRoute, private eventService:EventService){}
+
+  ngOnInit(): void {
+    this.route.params.subscribe((params)=>{
+      this.eventService.getEvent(params['id']).subscribe({
+        next: (event:Event)=>{
+            this.event=event
+            console.log(this.event)
+
+        },
+        error:(error)=>{
+          console.log(error)
+        }
+      })
+      })
+
+
+}
+
+
 
 }
