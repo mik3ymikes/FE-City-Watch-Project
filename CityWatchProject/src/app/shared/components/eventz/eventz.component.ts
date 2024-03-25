51bd07818072dfd1bef124f59db980c6ebfc8388
mik3ymikes <113144829+mik3ymikes@users.noreply.github.com>
2024-03-25 15:30:29 -0500

    workiong on dates

--- CityWatchProject/src/app/shared/components/eventz/eventz.component.ts
+++ CityWatchProject/src/app/shared/components/eventz/eventz.component.ts
@@ -161,7 +161,7 @@ getTime(dateTimeString: string): string {
   const minutes = date.getMinutes();
   const ampm = hours >= 12 ? 'PM' : 'AM';
 
-  
+
   hours = hours % 12;
   hours = hours ? hours : 12;
 
