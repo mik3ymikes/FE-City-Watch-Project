3487c9cc553949fef2f745525cbc2ed78f58f2e3
mik3ymikes <113144829+mik3ymikes@users.noreply.github.com>
2024-03-25 16:02:30 -0500

    setting up some editing for alerts

--- CityWatchProject/src/app/shared/components/alert/alert.component.ts
+++ CityWatchProject/src/app/shared/components/alert/alert.component.ts
@@ -1,6 +1,10 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Alert } from '../../models/alert';
 import { DatePipe, NgFor, NgStyle } from '@angular/common';
+import { User } from '../../models/user';
+import { UserService } from '../../../core/services/user.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { AlertService } from '../../../core/services/alert.service';
 
 @Component({
   selector: 'app-alert',
@@ -9,7 +13,37 @@ import { DatePipe, NgFor, NgStyle } from '@angular/common';
   templateUrl: './alert.component.html',
   styleUrl: './alert.component.css'
 })
-export class AlertComponent {
+export class AlertComponent implements OnInit {
+
+ currentUser: User | null = new User ({})
+
+
+ constructor(private userService: UserService, private alertService:AlertService,
+  private router:Router, private route:ActivatedRoute){}
+
+
+
+ngOnInit(): void {
+
+
+
+  this.userService.currentUserBehaviorSubject.subscribe(()=>{
+  this.currentUser=this.userService.currentUserBehaviorSubject.value
+
+  })
+
+}
+
+
+
+
+
+editPage(){
+  // this.router.navigate(['/edit-event', this.event.id])
+}
+
+
+
 @Input({required:true}) alert:Alert=new Alert({})
 
 
