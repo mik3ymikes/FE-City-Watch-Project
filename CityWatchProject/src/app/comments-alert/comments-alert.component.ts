2032c1f0ac5bfd35effacb128a12cf76d9a7c074
mik3ymikes <113144829+mik3ymikes@users.noreply.github.com>
2024-03-27 14:23:18 -0500

    setting up comments page for alerts

--- CityWatchProject/src/app/comments-alert/comments-alert.component.ts
+++ CityWatchProject/src/app/comments-alert/comments-alert.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Alert } from '../shared/models/alert';
+import { ActivatedRoute, Router } from '@angular/router';
+import { AuthenticationService } from '../core/services/authentication.service';
+import { AlertService } from '../core/services/alert.service';
 
 @Component({
   selector: 'app-comments-alert',
@@ -7,6 +11,27 @@ import { Component } from '@angular/core';
   templateUrl: './comments-alert.component.html',
   styleUrl: './comments-alert.component.css'
 })
-export class CommentsAlertComponent {
+export class CommentsAlertComponent implements OnInit {
+  alert:Alert = new Alert ()
 
-}
+  constructor(private authService:AuthenticationService, private route:ActivatedRoute,
+    private router:Router, private alertService:AlertService){}
+
+
+  ngOnInit(): void {
+    this.route.params.subscribe((params)=>{
+      this.alertService.getAlert(params['id']).subscribe({
+        next: (alert:Alert)=>{
+            this.alert=alert
+          },
+        error:(error)=>{
+          console.log(error)
+        }
+      })
+      })
+    }
+
+
+  }
+
+
