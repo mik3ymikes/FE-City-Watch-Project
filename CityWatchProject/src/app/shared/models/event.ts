cbd1ce9dad33a5a6eda9f4f16cec67ffdec5808e
mik3ymikes <113144829+mik3ymikes@users.noreply.github.com>
2024-04-01 11:22:47 -0500

    add comments and cleanup

--- CityWatchProject/src/app/shared/models/event.ts
+++ CityWatchProject/src/app/shared/models/event.ts
@@ -1,4 +1,5 @@
 import { User } from "./user";
+import { Comment } from "./comment";
 
 export class Event {
 
@@ -13,16 +14,7 @@ participants: User[]=[]
 title: string = '';
 cover_image_url: string = '';
 user?:User;
+comments:Comment []=[]
 
 
-// constructor(event:any){
-//   this.id=event.id || 0;
-//   this.content=event.content || "";
-//   this.start_date_time=event.start_date_time;
-//   this.end_date_time=event.end_date_time;
-//   this.created_at=event.created_at;
-//   this.user=event.user || new User({});
-//   this.title=event.title || "";
-//   this.cover_image_url=event.cover_image_url
-// }
 }
