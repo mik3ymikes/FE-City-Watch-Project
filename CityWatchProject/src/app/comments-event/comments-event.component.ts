337e95a7a67f03426edd61829e4b9d3b34857e2d
mik3ymikes <113144829+mik3ymikes@users.noreply.github.com>
2024-04-01 11:05:33 -0500

    setting up comments event

---
+++ CityWatchProject/src/app/comments-event/comments-event.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-comments-event',
+  standalone: true,
+  imports: [],
+  templateUrl: './comments-event.component.html',
+  styleUrl: './comments-event.component.css'
+})
+export class CommentsEventComponent {
+
+}
