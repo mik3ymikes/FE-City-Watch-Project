60e80016d0d90e255b7361f10d841e4f31404877
mik3ymikes <113144829+mik3ymikes@users.noreply.github.com>
2024-04-02 14:47:06 -0500

    clean up

--- CityWatchProject/src/app/features/eventz-display/eventz-display.component.ts
+++ CityWatchProject/src/app/features/eventz-display/eventz-display.component.ts
@@ -51,12 +51,6 @@ addEvent(){
 
 
 
-// get paginatedItems() {
-//   const startIndex = (this.currentPage - 1) * this.itemsPerPage;
-//   const endIndex = startIndex + this.itemsPerPage;
-//   return this.events.slice(startIndex, endIndex);
-// }
-
 onPageChange(pageNumber: number) {
   this.currentPage = pageNumber;
   this.router.navigate([], {
@@ -134,29 +128,3 @@ onPageChange(pageNumber: number) {
 
 
 
-
-//  this.route.queryParams.subscribe(params=>{
-  //     this.currentPage=params['page'] ? Number(params['page']) :1
-
-
-
-  // this.eventService.getEvents().subscribe({
-    //   next: (events:Event[])=>{
-      //     this.events=events
-      //     this.filteredEvents = events;
-  //     // console.log('get', this.events)
-  //   },
-  //   error: (error:any) =>{
-    //     console.error("Error fetching timeline events", error)
-    //   }
-    // })
-    // pages(): number[] {
-      //   const totalItems = this.events.length;
-      // const totalPages = Math.ceil(totalItems / this.itemsPerPage);
-      //   const pagesArray = Array.from({ length: totalPages }, (_, i) => i + 1);
-      //   return pagesArray;
-      // }
-
-    // totalPages(): number {
-    //   return this.pages().length;
-    // }
